f2b3caa42992b1fbcc8b86ed305d0d3c60abecdf
Khushbu Kumari <khushbu.k16997@gmail.com>
2024-08-10 17:42:57 +0530

    client changes

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 import { AppComponent } from './app/app.component';
+// import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+// This function bootstraps the Angular application with the root module (AppModule)
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
