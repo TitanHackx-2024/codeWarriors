1a0868f8c7146a79dfffc2eaebb3e3159dec7ab1
Nithin <nithinkorukonda132@gmail.com>
2024-08-10 11:37:11 +0530

    intial commit

---
+++ server/src/main/java/TitanHackx_2024/codeWarriors/BookYourChef/BookYourChefApplication.java
@@ -0,0 +1,13 @@
+package TitanHackx_2024.codeWarriors.BookYourChef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookYourChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookYourChefApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/TitanHackx_2024/codeWarriors/BookYourChef/BookYourChefApplicationTests.java
@@ -0,0 +1,13 @@
+package TitanHackx_2024.codeWarriors.BookYourChef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookYourChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
