d0976685baa223bf5a2bfff0f45a9ed75135f894
Nithin <nithinkorukonda132@gmail.com>
2024-08-10 13:19:46 +0530

    application properties

---
+++ server/src/main/java/server/controller/UserController.java
@@ -0,0 +1,24 @@
+package server.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import server.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+
+
+
+}
--- server/src/main/java/server/service/UserService.java
+++ server/src/main/java/server/service/UserService.java
@@ -1,4 +1,9 @@
 package server.service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
 }
