0fa4d0be0519cc899ae22f1d033318af179f8632
Khushbu Kumari <khushbu.k16997@gmail.com>
2024-08-10 17:42:57 +0530

    client changes

---
+++ client/src/app/components/chef/chef-details/chef-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chef-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './chef-details.component.html',
+  styleUrl: './chef-details.component.css'
+})
+export class ChefDetailsComponent {
+
+}
