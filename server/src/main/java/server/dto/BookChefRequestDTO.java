35c473a8e670c6e8e51a37c256406b007ecd48ed
romepatel <104779682+romepatel@users.noreply.github.com>
2024-08-11 15:00:35 +0530

    Booking changes
    Booking changes

--- server/src/main/java/server/dto/BookChefRequestDTO.java
+++ server/src/main/java/server/dto/BookChefRequestDTO.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 @Getter
 @Setter
 public class BookChefRequestDTO {
-    private List<UUID> chefSlot;
+    private UUID chefSlot;
     private List<UUID> chefSkills;
     private UUID chefId;
     private UUID userId;
